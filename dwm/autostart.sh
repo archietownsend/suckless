131dc645dd6b5eaf6e88496a9566290019ffb533
archietownsend <rccji@protonmail.com>
2023-12-14 10:46:45 +0000

    init

---
+++ dwm/autostart.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#brave &
+feh --bg-fill ~/.wallpaper.png &
+cd ~/.dwm/dwmblocks && ./dwmblocks &
+picom &
+wpctl set-mute @DEFAULT_AUDIO_SINK@ 1 &
+wpctl set-volume @DEFAULT_AUDIO_SINK@ 100% &
+numlockx
