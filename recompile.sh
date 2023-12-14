b83eda35aa384b72d32fa210ac5aedb994984a4f
Archie <archiet646@outlook.com>
2023-12-14 16:00:37 +0000

    add scratchpads!

--- recompile.sh
+++ recompile.sh
@@ -4,4 +4,4 @@ function runCommand() {
     for d in ./*/ ; do /bin/zsh -c "(cd "$d" && "$@")"; done
 }
 
-runCommand "cp config.def.h config.h && sudo make clean install"
+runCommand "sudo cp config.def.h config.h && sudo make clean install"
