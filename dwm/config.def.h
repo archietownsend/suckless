06295610c86112517d2e59a2132bcc428f411edc
archietownsend <rccji@protonmail.com>
2023-12-14 11:25:55 +0000

    recomplie script!

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -3,8 +3,8 @@
 #include <X11/XF86keysym.h>
 
 /* Constants */
-#define TERMINAL "alacritty"
-#define TERMCLASS "alacritty"
+#define TERMINAL "st"
+#define TERMCLASS "st"
 #define BROWSER "brave"
 
 /* appearance */
