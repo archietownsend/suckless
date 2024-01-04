77ead2138b0ecb9cb11e9cd10a0c2ca6693d5b95
Archie <archiet646@outlook.com>
2024-01-04 08:38:31 +0000

    remove keybind

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -84,7 +84,6 @@ static const Key keys[] = {
         { MODKEY|ShiftMask,             XK_w,      spawn,			SHCMD(BROWSER " --incognito") },
         { MODKEY,                       XK_space,  spawn,			{.v = dmenucmd } },
         { 0,         			XK_Print,  spawn,	  		SHCMD("sh ~/.dwm/scripts/screenshot") },
-	{ 0,				XK_F2,	   spawn, 	   		SHCMD("sh /usr/bin/sharepoint-search") },
 	{ 0, XF86XK_AudioMute,		spawn,	   SHCMD("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle; kill -44 $(pidof dwmblocks)") },
 	{ 0, XF86XK_AudioLowerVolume,	spawn,     SHCMD("wpctl set-mute @DEFAULT_AUDIO_SINK@ 0; wpctl set-volume @DEFAULT_AUDIO_SINK@ 0%+ && wpctl set-volume @DEFAULT_AUDIO_SINK@ 10%-; kill -44 $(pidof dwmblocks)") },
 	{ 0, XF86XK_AudioRaiseVolume,	spawn,SHCMD("wpctl set-mute @DEFAULT_AUDIO_SINK@ 0; wpctl set-volume @DEFAULT_AUDIO_SINK@ 0%- && wpctl set-volume @DEFAULT_AUDIO_SINK@ 10%+; kill -44 $(pidof dwmblocks)") },
