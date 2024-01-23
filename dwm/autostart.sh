c282d423e74b9fc880ec1438cbd40fd9df47742b
Archie <archiet646@outlook.com>
2024-01-23 15:34:49 +0000

    o

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -4,7 +4,7 @@ xwallpaper --zoom .wallpaper.jpg &
 wal -n -i $HOME/.wallpaper.jpg &
 cd ~/suckless/dwmblocks && ./dwmblocks &
 picom &
-wpctl set-mute @DEFAULT_AUDIO_SINK@ 1 &
 wpctl set-volume @DEFAULT_AUDIO_SINK@ 100% &
+wpctl set-mute @DEFAULT_AUDIO_SINK@ 1 &
 numlockx &
 wmname LG3D
