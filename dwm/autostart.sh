c6bf31c169a206dbb0618deb1aa9382f799a0ea1
archietownsend <rccji@protonmail.com>
2024-01-30 22:15:04 +0000

    Small changes

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -2,9 +2,9 @@
 brave &
 xwallpaper --zoom .wallpaper.jpg &
 wal -n -i $HOME/.wallpaper.jpg &
-cd ~/suckless/dwmblocks && ./dwmblocks &
-picom &
 wpctl set-volume @DEFAULT_AUDIO_SINK@ 100% &
 wpctl set-mute @DEFAULT_AUDIO_SINK@ 1 &
+cd ~/suckless/dwmblocks && ./dwmblocks &
+picom &
 numlockx &
 wmname LG3D
