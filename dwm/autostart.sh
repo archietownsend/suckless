e49e25b15cd01e755dfb9bf5d505aef24906ff65
archietownsend <rccji@protonmail.com>
2024-02-01 16:05:21 +0000

    remove mute

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -3,7 +3,6 @@ brave &
 xwallpaper --zoom .wallpaper.jpg &
 wal -n -i $HOME/.wallpaper.jpg &
 wpctl set-volume @DEFAULT_AUDIO_SINK@ 100% &
-wpctl set-mute @DEFAULT_AUDIO_SINK@ 1 &
 cd ~/suckless/dwmblocks && ./dwmblocks &
 picom &
 numlockx &
