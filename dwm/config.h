9f9342fdf5c3246c33a1352876ad014a33f07fa5
Archie Townsend <archiet646@outlook.com>
2024-03-01 19:50:50 +0000

    recomp

--- dwm/config.h
+++ dwm/config.h
@@ -29,11 +29,11 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
+	 *
 	 *	WM_NAME(STRING) = title
 	 */
        /* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
        { "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-       { "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
        { "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
        { NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
  };
