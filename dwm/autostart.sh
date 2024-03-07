06001fcb9ab7cbc08d86a1641f4c75e82f91c868
Archie Townsend <archiet646@outlook.com>
2024-03-07 07:31:50 +0000

    small changes

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -6,5 +6,6 @@ cd ~/suckless/dwmblocks && ./dwmblocks & #run dwmblocks
 picom & #run picom
 numlockx & #enable numlock
 wmname LG3D & #spoof wm name
-xset r rate 300 50 & 
+sh ~/.config/display.sh &
+xset r rate 300 50 &
 wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle; kill -44 $(pidof dwmblocks)
