826eddf59996d4c2df5f2612db648614b62808cb
archietownsend <rccji@protonmail.com>
2024-03-25 22:37:56 +0000

    autostart change

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -6,5 +6,6 @@ cd ~/suckless/dwmblocks && ./dwmblocks & #run dwmblocks
 xcompmgr -c & #run xcompmgr
 numlockx & #enable numlock
 wmname LG3D & #spoof wm name
-xset r rate 300 50 &
-wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle; kill -44 $(pidof dwmblocks)
+xset r rate 300 50
+#wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle; kill -44 $(pidof dwmblocks)
+#brave
