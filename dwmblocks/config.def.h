2f33cef2e5d247be19e71c229f26802af126b5fa
Archie <archiet646@outlook.com>
2024-03-27 15:49:52 +0000

    add btc price

--- dwmblocks/config.def.h
+++ dwmblocks/config.def.h
@@ -18,6 +18,7 @@
 
 // Define blocks for the status feed as X(cmd, interval, signal).
 #define BLOCKS(X)         \
+    X("sh ~/suckless/scripts/sb-price btc", 60, 7)  \
     X("sh ~/suckless/scripts/sb-weather", 6000, 12)  \
     X("sh ~/suckless/scripts/sb-pacman", 60, 11)  \
     X("sh ~/suckless/scripts/sb-internet", 5, 4)  \
