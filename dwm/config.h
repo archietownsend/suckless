8c871eba0a9c4002d987382c914784d25621879a
Archie <archiet646@outlook.com>
2024-03-27 11:45:38 +0000

    date script

--- dwm/config.h
+++ dwm/config.h
@@ -16,8 +16,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char dmenufont[]       = "JetBrainsMono Nerd Font:hack:size=10";
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10", "Symbols Nerd Font:size=10" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:hack:size=11";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=11", "Symbols Nerd Font:size=11" };
 /*static const char *colors[][3]      = {
                fg         bg         border
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
