fb55b96e8ec31b547f4e1ecf73380625ba8e053f
Archie <archiet646@outlook.com>
2024-03-27 11:36:34 +0000

    getting this to work...

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -2,11 +2,8 @@
 sh ~/.config/display.sh &
 xwallpaper --zoom ~/.wallpaper.jpg & #set wallpaper
 wal -n -i ~/.wallpaper.jpg & #set colour scheme to be from .wallpaper.jpg
-dwmblocks &
 xcompmgr -c & #run xcompmgr
 numlockx & #enable numlock
 wmname LG3D & #spoof wm name
 xset r rate 300 50 &
 dunst
-#wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle; kill -44 $(pidof dwmblocks)
-#brave
