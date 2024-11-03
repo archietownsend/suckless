265091e6c24aba5bacedb0899568c911f9a54167
archietownsend <rccji@protonmail.com>
2024-11-03 18:23:50 +0000

    move patches & update dmenu

--- dmenu/config.h
+++ dmenu/config.h
@@ -12,7 +12,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 
 /*static const char *colors[SchemeLast][2] = {
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#c1584e" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 */
@@ -24,3 +24,4 @@ static unsigned int lines      = 0;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
